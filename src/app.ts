1a882d726438d2de3e4f6d3bbb9bc6cfca4814c8
Bram-4197 <Bram-4197@protonmail.com>
2023-10-19 10:25:26 +0200

    Added mysql

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mysql = require("mysql")
+
+const connection = mysql.CreateConnection({
+  host: "",
+  user: "",
+  password: "",
+  database: "",
+})
+
+connection.connect()
+
 app.get('/', (req: any, res: any) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get("/Product/Get", (req: any, res:any) =>{
+
 })
+connection.end()
