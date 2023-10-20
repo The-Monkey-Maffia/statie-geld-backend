ee10d623753b81c728cead2ebcec6088d73a2cc4
Fredkr0ket <ferdovbalen@gmail.com>
2023-10-20 11:58:24 +0200

    added some default endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
-const express = require('express')
+import express from "express"
 const app = express()
 const port = 3000
 
-app.get('/', (req: any, res: any) => {
-  res.send('Hello World!')
+app.get('/goededoelen/:naam', () => {
+
 })
+
+app.post('/goededoelen', () => {
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
