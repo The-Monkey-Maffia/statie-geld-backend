1bc280e593e5bebaf3de4e2864006bc53584fa25
Fredkr0ket <ferdovbalen@gmail.com>
2023-11-02 09:24:32 +0100

    added env file and functionality

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import express from "express"
+import dotenv from 'dotenv';
+import { Database } from "./services/database";
+
 const app = express()
 const port = 3000
 
+dotenv.config();
+
+
+
+const database = new Database(
+  process.env.HOST!,
+  process.env.USER!,
+  process.env.PASSWORD!,
+  process.env.DATABASE!
+)
+const connection = database.connect()
+
 
 app.get('/get/goededoel/:naam', (req, res) => {
+  
 })
 
 app.post('/post/goededoelen', () => {
