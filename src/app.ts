ed8f74f28f963d3badfa4910e8a76519074a7ce2
DarkRL <sandervdg2005@gmail.com>
2023-11-06 10:17:32 +0100

    Added comments to api code

--- src/app.ts
+++ src/app.ts
@@ -23,9 +23,10 @@ const database = new Database(
 )
 const connection = database.connect()
 
-
+// GET: http://localhost:3000/get/goededoel/test
 app.get('/get/goededoel/:naam?', (req: express.Request, res: express.Response) => {
   if (!req.params.naam) {
+    // Get all from charity
     connection.query('SELECT *  FROM charity', (error, results) => {
       if (error) {
         res.status(500).json({ error: 'Error in query execution'})
@@ -36,6 +37,7 @@ app.get('/get/goededoel/:naam?', (req: express.Request, res: express.Response) =
   } else {
     const name = req.params.naam;
     console.log(name)
+    // Get selected charity by name from db
     connection.query('SELECT * FROM charity WHERE name = ?', [name], (error, results) => {
       if (error) {
         res.status(500).json({ error: 'Error in query execution' });
@@ -46,19 +48,24 @@ app.get('/get/goededoel/:naam?', (req: express.Request, res: express.Response) =
   }
 })
 
+
+// POST: http://localhost:3000/post/vote/
 app.post('/post/vote/', (req: express.Request, res: express.Response) => {
   const hardware_id = parseInt(req.body.hardware_id)
   const Vote_name = req.body.Vote_name
   console.log(hardware_id)
+  // Checks if hardware id already exists in DB
   connection.query("SELECT * FROM users where hardware_id = ?", [hardware_id], (error, results: RowDataPacket[])=>{
     console.log(results)
     if(results.length <= 0){
+      // Insert the hardware id in DB
       connection.query("INSERT INTO users (`hardware_id`) VALUES ('?')", [hardware_id], (error, results)=>{
         if(error){
           console.log(error)
           res.status(500).json({ error: 'Error in query execution by creating the user' });
           return;
         } else{
+          // Update vote on specific charity
           connection.query("UPDATE charity SET aantal_votes = aantal_votes + 1 WHERE name = ? ", [Vote_name], (error, results)=> {
             if(error){
               res.status(500).json({ error: 'Error in query execution' });
@@ -74,10 +81,12 @@ app.post('/post/vote/', (req: express.Request, res: express.Response) => {
   })
 })
 
+// GET: http://localhost:3000/get/drinks/1334679
 app.get('/get/drinks/:barcode_id', (req: express.Request, res: express.Response) => {
 
   const barcode_id = req.params.barcode_id;
   console.log(barcode_id)
+  // Retrieves barcode from db
   connection.query('SELECT * FROM products WHERE barcode_id = ?', [barcode_id], (error, results) => {
     console.log(error)
     if (error) {
