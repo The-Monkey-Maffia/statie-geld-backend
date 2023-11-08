0dfe7cc677126ba66a281388a1196b400740384e
Bram-4197 <Bram-4197@protonmail.com>
2023-11-08 10:58:12 +0100

    Fixed CORS Error

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import { Database } from "./services/database";
 import bodyParser from 'body-parser';
 import { RowDataPacket } from "mysql2";
-
+import { v4 as uuidv4 } from 'uuid';
 
 
 const app = express()
@@ -13,6 +13,13 @@ app.use(bodyParser.json());
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace this with your actual frontend's URL
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 dotenv.config();
 
 const database = new Database(
@@ -51,34 +58,55 @@ app.get('/get/goededoel/:naam?', (req: express.Request, res: express.Response) =
 
 // POST: http://localhost:3000/post/vote/
 app.post('/post/vote/', (req: express.Request, res: express.Response) => {
-  const hardware_id = parseInt(req.body.hardware_id)
+  const hardware_id = req.body.hardware_id
   const Vote_name = req.body.Vote_name
   console.log(hardware_id)
-  // Checks if hardware id already exists in DB
-  connection.query("SELECT * FROM users where hardware_id = ?", [hardware_id], (error, results: RowDataPacket[]) => {
-    console.log(results)
-    if (results.length <= 0) {
-      // Insert the hardware id in DB
-      connection.query("INSERT INTO users (`hardware_id`) VALUES ('?')", [hardware_id], (error, results) => {
-        if (error) {
-          console.log(error)
-          res.status(500).json({ error: 'Error in query execution by creating the user' });
-          return;
-        } else {
-          // Update vote on specific charity
-          connection.query("UPDATE charity SET aantal_votes = aantal_votes + 1 WHERE name = ? ", [Vote_name], (error, results) => {
-            if (error) {
-              res.status(500).json({ error: 'Error in query execution' });
-              return;
-            }
-            res.json({ data: results }); // Send the results as JSON
-          })
-        }
-      })
-    } else {
-      res.status(500).json({ error: 'you have already voted' });
-    }
-  })
+  if(!hardware_id){
+    const uinqiueID: string = uuidv4();
+    connection.query("INSERT INTO users (`hardware_id`) VALUES (?)", [uinqiueID], (error, results) => {
+      if (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Error in query execution by creating the user' });
+        return;
+      } else {
+        // Update vote on specific charity
+        connection.query("UPDATE charity SET aantal_votes = aantal_votes + 1 WHERE name = ? ", [Vote_name], (error, results) => {
+          if (error) {
+            res.status(500).json({ error: 'Error in query execution' });
+            return;
+          }
+          res.json({ data: results , uuid: uinqiueID}); // Send the results as JSON
+        })
+      }
+    })
+  } else{
+    connection.query("SELECT * FROM users where hardware_id = ?", [hardware_id], (error, results: RowDataPacket[]) => {
+      console.log(results)
+      if (results.length <= 0) {
+        // Insert the hardware id in DB
+        const uinqiueID: string = uuidv4();
+        connection.query("INSERT INTO users (`hardware_id`) VALUES (?)", [uinqiueID], (error, results) => {
+          if (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error in query execution by creating the user' });
+            return;
+          } else {
+            // Update vote on specific charity
+            connection.query("UPDATE charity SET aantal_votes = aantal_votes + 1 WHERE name = ? ", [Vote_name], (error, results) => {
+              if (error) {
+                res.status(500).json({ error: 'Error in query execution' });
+                return;
+              }
+              res.json({ data: results, uuid: uinqiueID}); // Send the results as JSON
+            })
+          }
+        })
+      } else {
+        res.status(500).json({ error: 'you have already voted'});
+      }
+    })
+  }
+
 })
 
 // GET: http://localhost:3000/get/drinks/1334679
